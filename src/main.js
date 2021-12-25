ee37b3dc72cbca18b517cf0b5ad5bc35d25474be
yangbaobing <337111893@qq.com>
2021-12-25 11:15:20 +0800

    线上地址更改

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios' // 引入axios
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import '@/styles/index.less' // 引入默认样式文件
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/'// 统一设置axios的请求头地址 baseURL,
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 统一设置axios的请求头地址 baseURL,
 Vue.prototype.$axios = 'axios'/// /所有的实例都直接共享拥有了 这个方法
 Vue.config.productionTip = false
 Vue.use(ElementUI)
